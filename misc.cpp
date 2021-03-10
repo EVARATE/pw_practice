b269fbbd2dd359917bd3103b43abf614ba7f8ed6
EVARATE <DaMoIsHere@protonmail.com>
2021-03-10 16:05:36 +0100

    Code cleanup

--- misc.cpp
+++ misc.cpp
@@ -3,7 +3,7 @@
 inline bool startsWith(std::string& str, std::string start){
     if(str.size() < start.size()){return false;}
 
-    for(int i = 0; i < start.size(); ++i){
+    for(int i = 0; i < (int)start.size(); ++i){
         if(start[i] != str[i]){
             return false;
         }
